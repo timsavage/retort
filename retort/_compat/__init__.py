960f3984b1a764127a96dd5a46fb6978f894cdf0
Tim Savage <tim@savage.company>
2017-02-01 21:31:06 +1100

    Moved py2 module into the _compat package

---
+++ retort/_compat/__init__.py
@@ -0,0 +1,48 @@
+"""
+Compatibility
+~~~~~~~~~~~~~
+
+Even though Lambda only officially supports Python 2.7 this library is being developed Python 3.x first. This module
+provides Python 2.7 fall-backs where possible.
+
+This library assumes Python 2.7 if the version is less than 3.
+
+"""
+from __future__ import unicode_literals
+import sys
+
+__all__ = (
+    'PY2', 'PY3',
+    'string_types', 'integer_types', 'text_type', 'binary_type',
+    'range',
+    'collections',
+)
+
+# Version helpers
+PY2 = sys.version_info[0] == 2
+PY3 = sys.version_info[0] == 3
+
+# Type helpers
+if PY3:
+    string_types = str,
+    integer_types = int,
+    text_type = str
+    binary_type = bytes
+else:
+    string_types = basestring,
+    integer_types = (int, long)
+    text_type = unicode
+    binary_type = str
+
+
+if PY2:
+    range = xrange
+else:
+    range = range
+
+# Fall backs from collections
+if PY3:
+    import collections
+    import urllib
+else:
+    from . import collections
